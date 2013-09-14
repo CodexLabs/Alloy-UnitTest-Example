2acb13b7d2a8a60d1eac6578713c680e62d53dc1
Andrew McElroy <sophrinix@gmail.com>
2013-09-13 20:33:58 -0500

    better explain-y comments

--- app/alloy.js
+++ app/alloy.js
@@ -10,6 +10,19 @@
 //
 // Alloy.Globals.someGlobalFunction = function(){};
 
+// If you use your ALLOY constants then when Alloy 'precompiles' down to Titanium
+// it will strip the code out and not even package it in the IPA or APK, etc file if it shouldn't be there
+// Documentation on the constants I am talking about: 
+// http://docs.appcelerator.com/titanium/3.0/#!/guide/Alloy_Controllers-section-34636384_AlloyControllers-ConditionalCode
+
+
+if(!OS_PRODUCTION)
+{
+	var behave = require('behave');	//require your created specs
+	require('specs/test-spec-1');	//run:tests
+	behave.run();
+}
+
 if(!OS_PRODUCTION))
 {
 	var behave = require('behave');
